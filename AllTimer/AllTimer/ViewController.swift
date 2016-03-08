4c98d8e72241c0cbe25b82f82515c98e48058318
ERII WATANABE <erii.watanabe@woodsmall.co.jp>
2016-03-08 16:06:19 +0900

    test

---
+++ AllTimer/AllTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllTimer
+//
+//  Created by ERII WATANABE on 2016/03/08.
+//  Copyright © 2016年 ERII WATANABE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AllTimer/AllTimer/counrdownViewController.swift
@@ -0,0 +1,13 @@
+//
+//  counrdownViewController.swift
+//  AllTimer
+//
+//  Created by ERII WATANABE on 2016/03/08.
+//  Copyright © 2016年 ERII WATANABE. All rights reserved.
+//
+
+import UIKit
+class countdownViewController: UIViewController {
+    
+    
+}
